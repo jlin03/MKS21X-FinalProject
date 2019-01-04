fafc7ed698aa3ddb3c1228ae5f63f38d074c29e0
jlin03 <jlin03@stuy.edu>
2019-01-04 10:10:45 -0500

    some code for isGameOver written, rowEq, colEq, diagEq code written

--- Board.java
+++ Board.java
@@ -1,8 +1,8 @@
 public class Board {
   private int[][] state;
 
-  public Board(int row, int col) {
-    state = new int[row][col];
+  public Board(int d) {
+    state = new int[d][d];
     clear();
   }
 
@@ -16,22 +16,90 @@ public class Board {
 
   public boolean move(int player, int posr, int posc) {
     if((player == 1 || player == 2) && posr >= 0 && posr < 3  && posc >= 0 && posc < 3) {
-      if(state[posr][posc] = 0) {
+      if(state[posr][posc] == 0) {
         state[posr][posc] = player;
         return true;
       }
       else {
-        
+
         return true;
       }
     }
     else {
       return false;
     }
-
-
-
   }
+
+  private boolean isGameOver() {
+    boolean over;
+    for(int r = 0;r < state.length;r++) {
+      for(int c = 0;c < state[r].length;c++) {
+        if(state[r][c] == 0) {
+          over = false;
+        }
+      }
+    }
+  }
+
+  private int rowEq() {
+    boolean equal = true;
+    for(int r = 0;r < state.length;r++) {
+      equal = true;
+      for(int c = 0;c < state[r].length;c++) {
+        if(state[r][0] != state[r][c] || state[r][c] = 0) {
+          equal = false;
+        }
+      }
+      if(equal) {
+        return r;
+      }
+    }
+    return -1;
+  }
+
+  private int colEq() {
+    boolean equal = true;
+    for(int c = 0;c < state[0].length;r++) {
+      equal = true;
+      for(int r = 0;r < state.length;c++) {
+        if(state[0][c] != state[r][c] || state[r][c] = 0) {
+          equal = false;
+        }
+      }
+      if(equal) {
+        return c;
+      }
+    }
+    return -1;
+  }
+
+  private int diagEq() {
+    boolean equal = true;
+    for(int r = 0;c < state.length;r++) {
+      equal = true;
+      for(int c = 0;c < state.length;c++) {
+        if(state[0][0] != state[r][c] || state[r][c] = 0) {
+          equal = false;
+        }
+      }
+      if(equal) {
+        return c;
+      }
+    }
+    for(int r = 0;c < state.length;r++) {
+      equal = true;
+      for(int c = 3;c < state.length;c--) {
+        if(state[0][3] != state[r][c] || state[r][c] = 0) {
+          equal = false;
+        }
+      }
+      if(equal) {
+        return c;
+      }
+    }
+    return -1;
+  }
+
 
 
 
