afc558f087908f96a656ab80f254aee0f2161544
jlin03 <jlin03@stuy.edu>
2019-01-06 13:25:35 -0500

    empty constructor written

--- AI.java
+++ AI.java
@@ -4,6 +4,10 @@ public class AI {
 	private ArrayList<int[][]> states;
 	private ArrayList<int[][]> weights;
 	
+	public AI() {
+		states = new ArrayList<int[][]>();
+		weights = new ArrayList<int[][]>();
+	}
 	
 	
 }
