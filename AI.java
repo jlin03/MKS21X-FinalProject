0193f8e93c560c8951455eddbcb8d93710fe8e5b
jlin03 <jlin03@stuy.edu>
2019-01-06 13:50:46 -0500

    preliminary code for export written

--- AI.java
+++ AI.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
 public class AI {
 	private ArrayList<int[][]> states;
 	private ArrayList<int[][]> weights;
@@ -9,5 +12,11 @@ public class AI {
 		weights = new ArrayList<int[][]>();
 	}
 	
+	public void export(String fileS, String fileW) throws IOException{
+		File state = new File(".", fileS);
+		File weight = new File(".", fileW);
+		state.createNewFile();
+		weight.createNewFile();
+	}
 	
 }
