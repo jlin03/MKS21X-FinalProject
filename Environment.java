026c2542bd2a01442e9b752f90fd4779ef01fd07
jlin03 <jlin03@stuy.edu>
2019-01-07 09:53:36 -0500

    constructor written

--- Environment.java
+++ Environment.java
@@ -2,7 +2,19 @@ public class Environment {
   Board game;
   AI p1;
   AI p2;
+  int learnRate = 0.7;
 
+  public Environment(int d, String fileS1, String fileW1, String fileS2, String fileW2) {
+    game = new Board(d);
+    p1 = new AI(fileS1,fileW1);
+    p2 = new AI(fileS2,fileW2);
+  }
+
+  public int learn() {
+
+
+
+  }
 
 
 
