e696900e843f6b7af4894f6d1a35699c6fe04bd6
jlin03 <jlin03@stuy.edu>
2019-01-07 19:18:03 -0500

    some test cases, not too important

--- test.java
+++ test.java
@@ -3,11 +3,14 @@ public class test {
   public static void main(String[] args) throws Exception{
 	int[][] game = {{1,2,1},
 					{1,2,1},
-					{2,1,2}};
+					{2,1,0}};
 	String tests = "[1, 2, 3];[2, 3, 4];[4, 5, 6]";
-  int[][] asc = {{1,2,3},{4,5,6},{7,8,9}};
-  Board test = new Board(3,game);
+	double[][] asc = {{1,2,3},{4,5,6},{7,8,9}};
+	double[][] weights = {{0.7816086952933228, 0.17269754442801744, 0.12121973511518369}, {0.3324462414019571, 0.5394673927771699, 0.024701116613785157}, {0.3714925195585371, 0.6802480648514232, 0.08136175235757104}};
+	Board test = new Board(3,game);
+	Environment training = new Environment(3,"p1States.txt","p1Weights.txt","p2States.txt","p2Weights.txt");
 
+	System.out.println(test.move(2,2,2));
 	System.out.println(test.isGameOver());
 	System.out.println(test);
 	System.out.println(test.winner());
@@ -20,11 +23,18 @@ public class test {
 	System.out.println(testAI.getWeights(game));
 	System.out.println(testAI.indexOfState(game));
 
-  for(int i = 1; i <= 9;i++) {
-    System.out.println(Arrays.toString(testAI.getNthMax(asc, i)));
-  }
+	for(int i = 9; i > 0;i--) {
+		System.out.println(Arrays.toString(testAI.getNthMax(weights, i)));
+	}
 
 
+
+	for(int i = 0; i < 50;i++) {
+		training.learn(true);
+	}
+	training.learn(true);
+	
+	
 
   }
 }
