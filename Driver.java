acd650a226de77ab08e1da87fa68a3f595edfe60
jlin03 <jlin03@stuy.edu>
2019-01-08 22:42:31 -0500

    some games initialized to test bots

--- Driver.java
+++ Driver.java
@@ -1,8 +1,21 @@
+import java.io.IOException;
+import java.io.FileNotFoundException;
 public class Driver {
   public static void main(String[] args) {
-
-
-
+	Environment training;
+	try {
+		training = new Environment(3,"p1States.txt","p1Weights.txt","p2States.txt","p2Weights.txt");
+		training.play(1);
+		training.play(2);
+		training.play(1);
+		training.play(2);
+		training.play(1);
+	}
+	catch (Exception e) {
+		System.out.println("File(s) not found");
+	}
+	
+	
   }
 
 }
