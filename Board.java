a6dc6acbaca57dccab8fbf0c6766a6356a9fc5db
jlin03 <jlin03@stuy.edu>
2019-01-10 09:54:35 -0500

    some documentation added

--- Board.java
+++ Board.java
@@ -1,16 +1,16 @@
 public class Board {
   private int[][] state;
 
-  public Board(int d) {
+  public Board(int d) {   //initializes a board with dimensions d * d
     state = new int[d][d];
     clear();
   }
 
-  public Board(int d, int[][] load) {
+  public Board(int d, int[][] load) {   //initializes a board with an already existing array
     state = load;
   }
 
-  public void clear() {
+  public void clear() {   //makes all values on the state array 0
     for(int r = 0;r < state.length;r++) {
       for(int c = 0;c < state[r].length;c++) {
         state[r][c] = 0;
@@ -18,7 +18,7 @@ public class Board {
     }
   }
 
-  public boolean move(int player, int posr, int posc) {
+  public boolean move(int player, int posr, int posc) {   //makes a move on the board, returns true if player is 1/2 and the position is a valid point on the state
     if(isMoveValid(player,posr,posc)) {
       state[posr][posc] = player;
       return true;
@@ -26,7 +26,7 @@ public class Board {
 	return false;
   }
 
-  public boolean isMoveValid(int player, int posr, int posc) {
+  public boolean isMoveValid(int player, int posr, int posc) {    // same as move(), but doesn't change the board
 	  if((player == 1 || player == 2) && posr >= 0 && posr < state.length  && posc >= 0 && posc < state.length) {
 		if(state[posr][posc] == 0) {
 		  return true;
@@ -35,7 +35,7 @@ public class Board {
 	  return false;
   }
 
-  public boolean isGameOver() {
+  public boolean isGameOver() {   //returns true if the game is a draw or a player has won
     boolean over = true;
     for(int r = 0;r < state.length;r++) {
       for(int c = 0;c < state[r].length;c++) {
@@ -50,7 +50,7 @@ public class Board {
     return over;
   }
 
-  private int rowEq() {
+  private int rowEq() {   //checks all rows and returns the index of a row where a player has all filled, returns -1 if there are no rows with this condition
     boolean equal = true;
     for(int r = 0;r < state.length;r++) {
       equal = true;
@@ -66,7 +66,7 @@ public class Board {
     return -1;
   }
 
-  private int colEq() {
+  private int colEq() {   //same as rowEq() but with cols
     boolean equal = true;
     for(int c = 0;c < state[0].length;c++) {
       equal = true;
@@ -82,7 +82,7 @@ public class Board {
     return -1;
   }
 
-  private int diagEq() {
+  private int diagEq() {    //same as rowEq() but with diags
     boolean equal = true;
     for(int r = 0;r < state.length;r++) {
       if(state[0][0] != state[r][r] || state[r][r] == 0) {
@@ -104,7 +104,7 @@ public class Board {
     return -1;
   }
 
-  public int winner() {
+  public int winner() {   //returns the winner of the game, 1 or 2, unless there is no winner, then it returns -1
 	  if(rowEq() != -1) {
 		  return state[rowEq()][0];
 	  }
@@ -117,7 +117,7 @@ public class Board {
 	  return -1;
   }
 
-  public double getReward(int player) {
+  public double getReward(int player) {   //returns -1 for the loser and 1 for the winner, depending on which player wins, returns -0.5 for a draw
 	  if(winner() == -1) {
 		  return 0;
 	  }
@@ -132,11 +132,11 @@ public class Board {
 	  return -0.5;
   }
 
-  public int[][] getState() {
+  public int[][] getState() {   //returns the state array
 	  return state;
   }
 
-  public String gameMessage() {
+  public String gameMessage() {   //prints the current status of the game
 	  if(isGameOver()) {
 		  if(winner() == -1) {
 			  return "The game has been completed. The result is a draw.";
@@ -148,7 +148,7 @@ public class Board {
 	  return "The game is in progress...";
   }
 
-  private String convert(int input) {
+  private String convert(int input) {   //converts 1 and 2 to X and O
 	if(input == 1) {
 	  return "X";
 	}
@@ -162,7 +162,7 @@ public class Board {
   }
 
 
-  public String toString() {
+  public String toString() {    //prints the board with the current state of the game
 	String out = "";
 	for(int r = 0;r < state.length-1;r++) {
 		for(int c = 0; c < state[r].length - 1;c++) {
